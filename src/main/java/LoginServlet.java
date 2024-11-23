13fe32015c75781ec21fb41fd19741a25a6e81a2
Isai Rajaraman <irajaram@usc.edu>
2024-11-23 14:02:05 -0800

    first commit from local

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/loginapp";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "" ;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                response.getWriter().println("Login Successful!");
+            } else {
+                response.getWriter().println("Invalid username or password.");
+            }
+
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("An error occurred: " + e.getMessage());
+        }
+    }
+}
+
